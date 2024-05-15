de6bfa66418775d5b478a3aecb00f5d53be5662a
Huseynbayramli <huseynbayramli002@gmail.com>
2024-05-15 11:26:15 +0400

    second

--- src/common/Pages/ProductInfo.jsx
+++ src/common/Pages/ProductInfo.jsx
@@ -5,10 +5,30 @@ import "react-rater/lib/react-rater.css";
 
 const ProductInfo = () => {
     const userID = JSON.parse(localStorage.getItem('user')).id;
-    console.log(userID);
-
     const [product, setProduct] = useState(null);
-    const [selectedColor, setSelectedColor] = useState('red');
+    const [selectedColor, setSelectedColor] = useState(null);
+    const productColorsData = [
+        {
+            id: 1,
+            name: "Black",
+            color: "black"
+        },
+        {
+            id: 2,
+            name: "Green",
+            color: "green"
+        },
+        {
+            id: 3,
+            name: "Red",
+            color: "red"
+        },
+        {
+            id: 4,
+            name: "Blue",
+            color: "blue"
+        }
+    ];
 
     useEffect(() => {
         const fetchProduct = async () => {
@@ -24,26 +44,23 @@ const ProductInfo = () => {
             fetchProduct();
         }
     }, [userID]);
+    console.log(userID)
+    const handleColorSelect = (color) => {
+        setSelectedColor(color);
+    };
 
     return (
         <div className='container w-[90%] m-auto mt-10'>
             {product ? (
                 <div className='flex justify-between h-[600px] gap-7'>
                     <div className='flex flex-col gap-7'>
-                        <div>
-                            {product.images[0] ? (<div className='w-40 h-32 object-center object-cover'> <img src={product.images[0]} className='w-full h-32' alt="" /></div>) : (<div></div>)}
-                        </div>
-                        <div>
-                            {product.images[1] ? (<div className='w-40 h-32 object-center object-cover'> <img src={product.images[1]} className='w-full h-32' alt="" /></div>) : (<div className='w-40 h-32 object-center object-cover'> <img src={product.images[0]} className='w-full h-32' alt="" /></div>)}
-                        </div>
-                        <div>
-                            {product.images[2] ? (<div className='w-40 h-32 object-center object-cover'> <img src={product.images[2]} className='w-full h-32' alt="" /></div>) : (<div className='w-40 h-32 object-center object-cover'> <img src={product.images[0]} className='w-full h-32' alt="" /></div>)}
-                        </div>
-                        <div>
-                            {product.images[3] ? (<div className='w-40 h-32 object-center object-cover'> <img src={product.images[3]} className='w-full h-32' alt="" /></div>) : (<div className='w-40 h-32 object-center object-cover'> <img src={product.images[0]} className='w-full h-32' alt="" /></div>)}
-                        </div>
+                        {product.images.slice(0, 4).map((image, index) => (
+                            <div key={index} className='w-40 h-32 object-center object-cover'>
+                                <img src={image} className='w-full h-32' alt={`Product ${index + 1}`} />
+                            </div>
+                        ))}
                     </div>
-                    <div className='w- bg-slate-300'>
+                    <div className=' bg-slate-300'>
                         <img src={product.images[0]} className='w-full h-[600px]' alt="" />
                     </div>
                     <div className='ml-10 w-1/4'>
@@ -55,11 +72,19 @@ const ProductInfo = () => {
                         <p className="text-2xl">
                             ${product.price}
                         </p>
-                        <p className='mt-6 text-sm'>{product.description}</p>
-                        <div className='w-full h-[1px] bg-gray-300 my-6'></div>
-                        <div className='flex gap-6 '>
+                        <p className='mt-6 text-sm border-b border-gray-300 pb-6'>{product.description}</p>
+                        <div className='flex gap-6 mt-6'>
                             <p className='text-xl'>Colors:</p>
-
+                            <div className="flex gap-3">
+                                {productColorsData.map((color) => (
+                                    <div
+                                        key={color.id}
+                                        className={`w-6 h-6 rounded-full cursor-pointer ${selectedColor === color.color ? 'border-[3px] border-gray-500' : ''}`}
+                                        style={{ backgroundColor: color.color }}
+                                        onClick={() => handleColorSelect(color.color)}
+                                    ></div>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
